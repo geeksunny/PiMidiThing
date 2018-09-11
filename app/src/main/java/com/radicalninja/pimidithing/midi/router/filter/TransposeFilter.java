5d30d4dd0f4b8e408ac9f8d381cd5dd1181fc16d
Justin Swanson <geeksunny@gmail.com>
2018-09-11 00:44:30 -0500

    TransposeFilter class

--- app/src/main/java/com/radicalninja/pimidithing/midi/router/filter/TransposeFilter.java
+++ app/src/main/java/com/radicalninja/pimidithing/midi/router/filter/TransposeFilter.java
@@ -1,9 +1,21 @@
 package com.radicalninja.pimidithing.midi.router.filter;
 
+import android.support.annotation.IntRange;
+
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.radicalninja.pimidithing.midi.MidiMessage;
+import com.radicalninja.pimidithing.util.MathUtils;
 
 public class TransposeFilter extends BaseFilter {
+
+    private static final String KEY_STEP = "step";
+
+    private static final int NOTE_MIN = 0;
+    private static final int NOTE_MAX = 127;
+    private static final int NOTE_STEP = 12;
+
+    private int step;
 
     public TransposeFilter(JsonObject settings) {
         super(settings);
@@ -11,19 +23,38 @@ public class TransposeFilter extends BaseFilter {
 
     @Override
     public void onSettings(JsonObject settings) {
-        // TODO
+        final JsonElement stepJson = settings.get(KEY_STEP);
+        if (null != stepJson && stepJson.isJsonPrimitive()) {
+            setStep(stepJson.getAsInt());
+        }
     }
 
     @Override
     public JsonObject getSettings() {
-        // TODO
-        return null;
+        final JsonObject json = new JsonObject();
+        json.addProperty(KEY_STEP, step);
+        return json;
     }
 
     @Override
     Result onProcess(MidiMessage message) {
-        // TODO!!!
-        return null;
+        if (message.hasProperty(MidiMessage.PROPERTY_NAME_NOTE)) {
+            final int note = message.getProperty(MidiMessage.PROPERTY_NAME_NOTE);
+            final int scaled = note + (step * NOTE_STEP);
+            final int _note = MathUtils.clipToRange(scaled, NOTE_MIN, NOTE_MAX);
+            if (note != _note) {
+                message.setProperty(MidiMessage.PROPERTY_NAME_NOTE, _note);
+            }
+        }
+        return new Result(message);
+    }
+
+    void setStep(@IntRange(from=-10, to=10) final int step) {
+        if (step > 10 || step < -10) {
+            this.step = 0;
+        } else {
+            this.step = step;
+        }
     }
 
 }
